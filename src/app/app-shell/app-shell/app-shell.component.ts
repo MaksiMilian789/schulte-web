c53c197e8db6a4df258e249aa9ecc7c3055aa151
Maksim Kozyrin <kme@sinetic.ru>
2022-10-08 14:21:10 +0700

    feat: добавлено оформление app-shell

--- src/app/app-shell/app-shell/app-shell.component.ts
+++ src/app/app-shell/app-shell/app-shell.component.ts
@@ -1,22 +1,33 @@
-import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
-import { MatSnackBar } from '@angular/material/snack-bar';
+import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { map, Observable } from 'rxjs';
 
 @Component({
-  selector: 'app-app-shell',
   templateUrl: './app-shell.component.html',
-  styleUrls: ['./app-shell.component.scss']
+  styleUrls: ['./app-shell.component.scss'],
 })
-export class AppShellComponent implements OnInit {
+export class AppShellComponent {
+  fio = "admin";
 
+  constructor( private router: Router) {
 
-  constructor(
-  ) {
   }
 
-  ngOnInit(): void {
+  logout(): void {
+    //this.authService.logout();
+    this.router.navigate(['/auth']);
   }
-  
 
+  private _formatFio(fio: string): string {
+    let result = '';
+    const parts = fio.split(' ');
+    result += parts[0];
+    if (parts.length > 1) {
+      result += ' ' + parts[1][0] + '.';
+    }
+    if (parts.length > 2) {
+      result += ' ' + parts[2][0] + '.';
+    }
+    return result;
+  }
 }
