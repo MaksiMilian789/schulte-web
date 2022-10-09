e9add015800c870ee622e67af7f33aedee3f3a52
Maksim Kozyrin <maksim.kozyrin789@mail.ru>
2022-10-09 13:26:37 +0700

    feat: добавлено пересоздание матрицы и количество этапов

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -7,17 +7,36 @@ import { MatSnackBar } from '@angular/material/snack-bar';
   styleUrls: ['./test.component.scss'],
 })
 export class TestComponent {
+  //перемешанная матрица кнопок
   matrix: number[][] = [];
+
+  //шаблон верной последовательности
   sequence: number[] = [];
 
-  k = 0;
-  color = '';
+  //индекс в шаблоне
+  k: number = 0;
+
+  //цветовая индикация
+  color: string = '';
+
+  disabled: boolean = true;
+
+  stage: number = 1;
 
   constructor(private _snackbar: MatSnackBar) {
+    this.buildSequence();
+    this.buildMatrix();
+  }
+
+  buildSequence(): void {
+    this.sequence = [];
     for (let i = 1; i < 26; i++) {
       this.sequence.push(i);
     }
+  }
 
+  buildMatrix(): void {
+    this.matrix = [];
     let random = Array.from(this.sequence);
     random.sort(() => Math.random() - 0.5);
 
@@ -31,9 +50,31 @@ export class TestComponent {
     }
   }
 
+  start(): void {
+    this.buildSequence();
+    this.buildMatrix();
+    this.disabled = false;
+  }
+
+  rebuild(): void {
+    this.buildSequence();
+    this.buildMatrix();
+  }
+
   checkButton(number: any): void {
     if (this.sequence[this.k] == number) {
       this.sequence = this.sequence.slice(1);
+      if (number == 25) {
+        this.color = 'primary';
+        setTimeout(() => {
+          this.color = '';
+        }, 250);
+        if(this.stage == 5) {
+          this.disabled = true;
+        }        
+        this.stage++;
+        this.rebuild();
+      }
     } else {
       this.color = 'warn';
       setTimeout(() => {
