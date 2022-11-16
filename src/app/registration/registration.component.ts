322d550388883b10aa557628682e109702e16cdf
MaksiMilian789 <maksim.kozyrin789@mail.ru>
2022-11-16 11:30:32 +0700

    feat: доработана регистрация

--- src/app/registration/registration.component.ts
+++ src/app/registration/registration.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 import { RegistrationService } from './registration.service';
 
@@ -12,25 +13,33 @@ export class RegistrationComponent {
   form: FormGroup;
   hide = true;
 
-  constructor(private _registration: RegistrationService) {
+  constructor(
+    private _registration: RegistrationService,
+    private _snackbar: MatSnackBar
+  ) {
     this.form = new FormGroup({
       login: new FormControl('', Validators.required),
-      password: new FormControl('', Validators.required),
-      passwordRepeat: new FormControl('', this._passwordRepeatValidator),
+      password: new FormControl('', [
+        Validators.required,
+        Validators.minLength(6),
+      ]),
+      passwordRepeat: new FormControl('', [
+        Validators.required,
+        Validators.minLength(6),
+      ]),
     });
   }
 
   registration(): void {
-    //проверка в базе по Http
-
-    this._registration.registration(
-      this.form.value.login,
-      this.form.value.password,
-      this.form.value.passwordRepeat
-    );
-  }
-
-  private _passwordRepeatValidator(control: any) {
-    return { isEqual: control.value === this.form.controls['password'].value };
+    if (this.form.value.password != this.form.value.passwordRepeat) {
+      this._snackbar.open('Введённые пароли не совпадают');
+    } else {
+      //проверка в базе по Http
+      this._registration.registration(
+        this.form.value.login,
+        this.form.value.password,
+        this.form.value.passwordRepeat
+      );
+    }
   }
 }
--- src/app/registration/registration.service.ts
+++ src/app/registration/registration.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { Router } from '@angular/router';
 
 import { Subject } from 'rxjs';
@@ -9,14 +10,16 @@ import { Subject } from 'rxjs';
 export class RegistrationService {
   registrationMessage: Subject<boolean> = new Subject<boolean>();
 
-  constructor(private _router: Router) {}
+  constructor(private _router: Router, private _snackbar: MatSnackBar) {}
 
   registration(login: string, password: string, passwordRepeat: string): void {
     if (this.httpReg(login, password, passwordRepeat)) {
       this._router.navigate(['/auth']);
       this.registrationMessage.next(true);
+      this._snackbar.open('Регистрация успешна');
     } else {
       this.registrationMessage.next(false);
+      this._snackbar.open('Такой логин уже существует');
     }
   }
 
